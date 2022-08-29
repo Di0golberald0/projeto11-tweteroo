3bd3ca8d722f43018f1766ff4bc950b5ead8ad05
Diogo Lima Beraldo <diogolberaldo@gmail.com>
2022-08-29 17:05:04 -0300

    Versao de Entrega

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 import express from 'express';
-import cors from 'cors';
 
 const server = express();
 server.use(express.json());
 
-const usuario = 
-    {
-        username: 'bobesponja', 
-        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
-    }
+const usuario = {
+    username: "",
+    avatar: ""
+}
 
 const tweets = []
 
 server.post('/sign-up', (req, res) => {
     const login = req.body;
     usuario.push(login);
-    res.send(usuario);
+    res.send("OK");
   });
 
 server.post('/tweets', (req, res) => {
-    res.send(tweets);
+    const NovoTweet = req.body;
+    tweets.push(...tweets,
+        {
+            username: `${NovoTweet.username}`,
+            tweet: `${NovoTweet.tweet}`
+        }
+    );
+    if(tweets.lenght >= 10){
+        tweets.filter((index) => tweets.index !== 0)
+    }
+    res.send("OK");
   });
 
 server.get('/tweets', (req, res) => {
