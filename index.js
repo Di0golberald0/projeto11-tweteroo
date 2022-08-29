f16d8cb5b9427ee9dba4d45a733e3085283b9f50
Diogo Lima Beraldo <diogolberaldo@gmail.com>
2022-08-29 14:59:54 -0300

    Post Usuario

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
 import express from 'express';
+import cors from 'cors';
 
 const server = express();
+server.use(express.json());
+
+const usuario = 
+    {
+        username: 'bobesponja', 
+        avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
+    }
+
+const tweets = []
+
+server.post('/sign-up', (req, res) => {
+    const login = req.body;
+    usuario.push(login);
+    res.send(usuario);
+  });
+
+server.post('/tweets', (req, res) => {
+    res.send(tweets);
+  });
+
+server.get('/tweets', (req, res) => {
+    res.send(tweets);
+  });
 
 server.listen(5000);
